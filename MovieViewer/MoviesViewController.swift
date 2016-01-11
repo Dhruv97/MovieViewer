d4ba3a557eec659fe2a1540b47b6fd6f43b599bd
Dhruv Upadhyay <Dhruv@Dhruvs-MacBook-Pro.local>
2016-01-11 13:46:56 -0500

    Initial commit

--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -7,22 +7,49 @@
 //
 
 import UIKit
-import  AFNetworking
+import EZLoadingActivity  //EZLoadingActivity.show("Loading...", disableUI: true) Not sure where to put this line of code...
+import AFNetworking
+
+
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet var tableView: UITableView!
     
+   
+    
+    var refresher: UIRefreshControl!
+
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+
     var movies: [NSDictionary]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+      /*  activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0, 0, 50, 50))
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+        UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+        
+        refresher = UIRefreshControl()
+        
+        refresher.attributedTitle = NSAttributedString(string: "Pull to refresh")
+        
+        refresher.addTarget(self, action: "refresh", forControlEvents: UIControlEvents.ValueChanged)
+        
+        self.tableView.addSubview(refresher)
+        
+        refresh()*/
 
         tableView.dataSource = self
         tableView.delegate = self
         
-        
-        
+       
+      EZLoadingActivity.show("Loading...", disableUI: true)
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        
         let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
         let request = NSURLRequest(URL: url!)
         let session = NSURLSession(
@@ -31,23 +58,51 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             delegateQueue:NSOperationQueue.mainQueue()
         )
         
+      
         let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
             completionHandler: { (dataOrNil, response, error) in
                 if let data = dataOrNil {
+                      EZLoadingActivity.hide(success: true, animated: true) 
+                    
                     if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
                         data, options:[]) as? NSDictionary {
+                            
                             NSLog("response: \(responseDictionary)")
                             
-                           self.movies = responseDictionary["results"] as! [NSDictionary]
+                            
+                            
+                            self.movies = responseDictionary["results"] as! [NSDictionary]
+                            
                             self.tableView.reloadData()
+                           
+                            
+                            
+                            
                     }
                 }
+                
+                
+
         });
-        task.resume()
+
         
+       
+task.resume()
+       
         
     }
-
+/*
+    
+  /  func refresh() {
+    
+        
+        self.tableView.reloadData()
+        
+        self.refresher.endRefreshing()
+        
+    }*/
+  
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -84,19 +139,26 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         cell.posterView.setImageWithURL(imageURL!)
         
-        
+      
         return cell
         
     }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+       
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPathForCell(cell)!
+        
+        let movie = movies![indexPath.row]
+        
+        let movieDetailsViewController = segue.destinationViewController as! MovieDetailsViewController
+        movieDetailsViewController.movie = movie
+        
     }
-    */
+    
 
 }
